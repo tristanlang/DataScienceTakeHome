f13cb80a10cb0241fa360803e49661d1b3d86a3f
tristanjlang <tristan@alum.mit.edu>
2014-05-22 19:06:46 -0400

    have the dataframes set up, now need to get counts and conversion rates

--- airbnb2.py
+++ airbnb2.py
@@ -39,10 +39,38 @@ t_new_page = (new_conv_rate - old_conv_rate) / stdev
 print(total_conv_rate, stdev, t_new_page)
 
 
-##
+####
 
 
 # concatenate unique list with the unique version of the duplicates
 country = pd.read_csv('country.csv').drop_duplicates()
 experiment = pd.merge(experiment, pd.read_csv('country.csv'), on='user_id').drop_duplicates()
-print(experiment.shape)
+#print(experiment)
+
+'''
+NEXT STEPS:
+-perform same sort of duplicate and unique counts and conversion sums for the datasets with country
+-group by country
+-look at conv rates and counts by country
+'''
+duplicates = experiment.groupby(['user_id']).filter(lambda x: len(x) > 1)
+duplicate_conversions = duplicates.groupby(['landing_page', 'country']).sum() # new = 0, old = 501
+duplicate_counts = duplicates.groupby(['landing_page', 'country']).count() # new = 4761, old = 4759
+
+uniques = experiment.groupby(['user_id']).filter(lambda x: len(x) == 1)
+unique_conversions = uniques.groupby(['landing_page', 'country']).sum() # new = 9527, old = 9049
+unique_counts = uniques.groupby(['landing_page', 'country']).count() # new = 90815, old = 90813
+
+'''old_count = unique_counts.ix['new_page', 'converted'] + duplicate_counts.ix['new_page', 'converted']
+old_conv = unique_conversions.ix['old_page', 'converted'] + duplicate_conversions.ix['old_page', 'converted']
+new_count = unique_counts.ix['new_page', 'converted']
+new_conv = unique_conversions.ix['new_page', 'converted']
+
+old_conv_rate = (unique_conversions.ix['old_page', 'converted'] + duplicate_conversions.ix['old_page', 'converted']) / (unique_counts.ix['new_page', 'converted'] + duplicate_counts.ix['new_page', 'converted'])
+new_conv_rate = unique_conversions.ix['new_page', 'converted'] / unique_counts.ix['new_page', 'converted']
+
+total_conv_rate = (old_conv + new_conv) / (old_count + new_count)
+stdev = (total_conv_rate * (1 - total_conv_rate) / (old_count + new_count))**0.5
+t_new_page = (new_conv_rate - old_conv_rate) / stdev
+
+print(total_conv_rate, stdev, t_new_page)'''
